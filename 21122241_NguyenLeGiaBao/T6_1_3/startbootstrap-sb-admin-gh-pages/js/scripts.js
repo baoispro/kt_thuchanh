d00d7d549c5c705f57e210b7f82476b15bdd7245
baoispro <nguyenlegiabao810@gmail.com>
2023-05-05 07:39:48 +0700

    hoan thanh cau 1

--- 21122241_NguyenLeGiaBao/T6_1_3/startbootstrap-sb-admin-gh-pages/js/scripts.js
+++ 21122241_NguyenLeGiaBao/T6_1_3/startbootstrap-sb-admin-gh-pages/js/scripts.js
@@ -1,9 +1,9 @@
 /*!
-    * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
-    * Copyright 2013-2023 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
-    */
-    // 
+ * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+ */
+// 
 // Scripts
 // 
 
@@ -24,3 +24,38 @@ window.addEventListener('DOMContentLoaded', event => {
     }
 
 });
+
+// var button = document.getElementById("taoTK")
+// button.addEventListener("click", function(event) {
+//     event.preventDefault(); // Ngăn chặn form gửi dữ liệu đi
+
+//     // Lấy đối tượng form và các trường dữ liệu
+//     var form = document.getElementById("form");
+//     var fn = form.querySelector('input[type="text"]:nth-of-type(1)');
+//     var ln = form.querySelector('input[type="text"]:nth-of-type(2)');
+//     var email = form.querySelector('input[type="email"]');
+//     var p = form.querySelector('input[type="password"]:nth-of-type(1)');
+//     var p1 = form.querySelector('input[type="password"]:nth-of-type(1)');
+//     var chuyenkhoa = form.querySelector('select[name="select"]');
+
+//     // Kiểm tra mã bệnh nhân
+//     var regexMaBN = /^[0-9]{n}$/;
+//     if (regexMaBN.test(fn.value)) {
+//         window.alert("Không được nhập số")
+//         console.log("h")
+//         return;
+//     }
+
+//     // if (regexMaBN.test(ln.value)) {
+//     //     window.alert("Không được nhập số")
+//     //     return;
+//     // }
+
+//     // Kiểm tra mật khẩu
+//     // if (mk.value.length < 6) {
+//     //     mk.nextElementSibling.textContent = '(*) Vui lòng nhập mật khẩu có ít nhất 6 ký tự';
+//     //     return;
+//     // } else {
+//     //     mk.nextElementSibling.textContent = '';
+//     // }
+// });
